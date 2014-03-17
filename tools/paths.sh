0b33178f0a10d7cd19a77bcd59310074c16ed3a4
kaoru <kaorut@mc.pikara.ne.jp>
2014-03-18 00:26:15 +0900

    Change the file name

--- tools/paths.sh
+++ tools/paths.sh
@@ -9,10 +9,6 @@ list_sources()
         $1/bobura/src \
         $1/bobura_model/include \
         $1/setup/src \
-        $1/tetengo2/include \
-        $1/tetengo2/src \
-        $1/tetengo2_detail/include \
-        $1/tetengo2_gui/include \
         -maxdepth 1 \
         -type f \
         -name '*.h' -or \
@@ -24,8 +20,6 @@ list_test_sources()
     find \
         $1/bobura/test \
         $1/bobura_model/test \
-        $1/tetengo2/test \
-        $1/tetengo2_gui/test \
         -maxdepth 1 \
         -type f \
         -name '*.h' -or \
