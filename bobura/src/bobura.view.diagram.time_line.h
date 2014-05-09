26e0f663d21f39ad1180a286381351f1795f9af5
kaoru <kaorut@mc.pikara.ne.jp>
2014-05-09 15:47:10 +0900

    Replace destructor implementation to TETENGO2_STDALT_DESTRUCTOR_DEFAULT_IMPLEMENTATION

--- bobura/src/bobura.view.diagram.time_line.h
+++ bobura/src/bobura.view.diagram.time_line.h
@@ -113,7 +113,7 @@ namespace bobura { namespace view { namespace diagram
             \brief Destroys the time line.
         */
         virtual ~time_line()
-        TETENGO2_STDALT_NOEXCEPT = default;
+        TETENGO2_STDALT_DESTRUCTOR_DEFAULT_IMPLEMENTATION
 
 
         // functions
