4b71dec33065194ce8d9e85ce5eb663aec316f7a
kaoru <kaorut@mc.pikara.ne.jp>
2014-11-26 06:39:19 +0900

    Implement test_bobura::oudia_diagram_dialog #88 (wip)

--- bobura_core/test/test_bobura.oudia_diagram_dialog.cpp
+++ bobura_core/test/test_bobura.oudia_diagram_dialog.cpp
@@ -8,6 +8,9 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <tetengo2.h>
+
+#include <bobura/oudia_diagram_dialog.h>
 #include <bobura/type_list.h>
 
 
@@ -25,6 +28,15 @@ namespace
 
     using traits_type_list_type = bobura::type_list::traits<detail_type_list_type>;
 
+    using string_type = common_type_list_type::string_type;
+
+    using window_type = ui_type_list_type::window_type;
+
+    using message_catalog_type = locale_type_list_type::message_catalog_type;
+
+    using oudia_diagram_dialog_type =
+        bobura::oudia_diagram_dialog<traits_type_list_type::dialog_type, common_type_list_type::size_type>;
+
 
 }
 
@@ -37,21 +49,34 @@ BOOST_AUTO_TEST_SUITE(oudia_diagram_dialog)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type parent{};
+        const message_catalog_type message_catalog{};
+        const oudia_diagram_dialog_type oudia_diagram_dialog{ parent, message_catalog };
     }
 
     BOOST_AUTO_TEST_CASE(file_name)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type parent{};
+        const message_catalog_type message_catalog{};
+        const oudia_diagram_dialog_type oudia_diagram_dialog{ parent, message_catalog };
+
+        BOOST_CHECK(oudia_diagram_dialog.file_name().empty());
     }
 
     BOOST_AUTO_TEST_CASE(set_file_name)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type parent{};
+        const message_catalog_type message_catalog{};
+        oudia_diagram_dialog_type oudia_diagram_dialog{ parent, message_catalog };
+
+        oudia_diagram_dialog.set_file_name(string_type{ TETENGO2_TEXT("hoge") });
+
+        const string_type expected{ TETENGO2_TEXT("hoge") };
+        BOOST_CHECK(oudia_diagram_dialog.file_name() == expected);
     }
 
     BOOST_AUTO_TEST_CASE(names)
