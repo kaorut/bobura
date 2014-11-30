ac2da625aad0d13a31a55ee452461dd75ee011d7
kaoru <kaorut@mc.pikara.ne.jp>
2014-12-01 02:45:08 +0900

    Implement test_bobura::command::set_horizontal_scale #88

--- bobura_core/test/test_bobura.command.set_horizontal_scale.cpp
+++ bobura_core/test/test_bobura.command.set_horizontal_scale.cpp
@@ -8,6 +8,9 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <bobura/command/set_horizontal_scale.h>
+#include <bobura/diagram_view.h>
+#include <bobura/timetable_model.h>
 #include <bobura/type_list.h>
 
 
@@ -25,6 +28,33 @@ namespace
 
     using traits_type_list_type = bobura::type_list::traits<detail_type_list_type>;
 
+    using model_type =
+        bobura::timetable_model<
+            common_type_list_type::size_type,
+            common_type_list_type::difference_type,
+            common_type_list_type::string_type,
+            common_type_list_type::operating_distance_type,
+            common_type_list_type::speed_type,
+            ui_type_list_type::fast_font_type
+        >;
+
+    using message_catalog_type = locale_type_list_type::message_catalog_type;
+
+    using view_traits_type = traits_type_list_type::view_type;
+
+    using view_type = bobura::diagram_view<view_traits_type>;
+
+    using scale_type = common_type_list_type::scale_type;
+
+    using set_horizontal_scale_command_type =
+        bobura::command::set_horizontal_scale<
+            traits_type_list_type::command_type,
+            scale_type,
+            traits_type_list_type::command_set_type,
+            traits_type_list_type::main_window_type,
+            view_traits_type
+        >;
+
 
 }
 
@@ -38,7 +68,10 @@ BOOST_AUTO_TEST_SUITE(set_horizontal_scale)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        model_type model{};
+        const message_catalog_type message_catalog{};
+        view_type diagram_view{ model, message_catalog };
+        const set_horizontal_scale_command_type command{ diagram_view, scale_type{ 42 } };
     }
 
 
