c20adbf2b9aa716fe58f28023d866f8adb4c0985
kaoru <kaorut@mc.pikara.ne.jp>
2014-12-11 03:58:58 +0900

    Skip a segmentation fault on Cygwin #91

--- bobura_core/test/test_bobura.view.diagram.station_line.cpp
+++ bobura_core/test/test_bobura.view.diagram.station_line.cpp
@@ -170,6 +170,11 @@ BOOST_AUTO_TEST_SUITE(station_line)
         station_line1 = std::move(station_line2);
     }
 
+// This test case causes a segmentation fault on Cygwin.
+#if !( \
+    __CYGWIN__ /*BOOST_OS_CYGWIN*/ && \
+    (BOOST_COMP_GNUC >= BOOST_VERSION_NUMBER(4, 8, 0) && BOOST_COMP_GNUC < BOOST_VERSION_NUMBER(4, 9, 0)) \
+)
     BOOST_AUTO_TEST_CASE(draw_on)
     {
         BOOST_TEST_PASSPOINT();
@@ -196,6 +201,7 @@ BOOST_AUTO_TEST_SUITE(station_line)
         const auto p_canvas = window.create_canvas();
         station_line.draw_on(*p_canvas);
     }
+#endif
 
     BOOST_AUTO_TEST_CASE(p_item_by_position)
     {
