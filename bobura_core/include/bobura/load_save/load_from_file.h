c6eb245f1b454a20533e20f2b9e3c65dc50a7fd2
kaoru <kaorut@mc.pikara.ne.jp>
2015-03-13 03:54:04 +0900

    Move the implementation of bobura::load_save::load_from_file to .cpp #99

--- bobura_core/include/bobura/load_save/load_from_file.h
+++ bobura_core/include/bobura/load_save/load_from_file.h
@@ -9,26 +9,14 @@
 #if !defined(BOBURA_LOADSAVE_LOADFROMFILE_H)
 #define BOBURA_LOADSAVE_LOADFROMFILE_H
 
-#include <cassert>
-#include <ios>
-#include <iterator>
 #include <memory>
-#include <stdexcept>
-#include <utility>
 
 #include <boost/filesystem.hpp>
-#include <boost/filesystem/fstream.hpp>
 #include <boost/optional.hpp>
-#include <boost/spirit/include/support_multi_pass.hpp>
-#include <boost/throw_exception.hpp>
 
 #include <tetengo2.h>
 
 #include <bobura/load_save/confirm_file_save.h>
-#include <bobura/model/serializer/exec_json_reading_task.h>
-#include <bobura/model/serializer/reader_selector.h>
-#include <bobura/model/serializer/reader_set.h>
-#include <bobura/model/serializer/select_oudia_diagram.h>
 #include <bobura/timetable_model.h>
 
 
@@ -57,12 +45,6 @@ namespace bobura { namespace load_save
         //! The string type.
         using string_type = typename traits_type::string_type;
 
-        //! The iterator type.
-        using iterator = typename traits_type::iterator;
-
-        //! The output stream type.
-        using output_stream_type = typename traits_type::output_stream_type;
-
         //! The operating distance type.
         using operating_distance_type = typename traits_type::operating_distance_type;
 
@@ -75,35 +57,8 @@ namespace bobura { namespace load_save
         //! The abstract window type.
         using abstract_window_type = typename traits_type::abstract_window_type;
 
-        //! The message box type.
-        using message_box_type = typename traits_type::message_box_type;
-
-        //! The file open dialog type.
-        using file_open_dialog_type = typename traits_type::file_open_dialog_type;
-
-        //! The file save dialog type.
-        using file_save_dialog_type = typename traits_type::file_save_dialog_type;
-
-        //! The dialog type.
-        using dialog_type = typename traits_type::dialog_type;
-
-        //! The timer type.
-        using timer_type = typename traits_type::timer_type;
-
-        //! The system color set type.
-        using system_color_set_type = typename traits_type::system_color_set_type;
-
-        //! The OuDia diagram dialog type.
-        using oudia_diagram_dialog_type = typename traits_type::oudia_diagram_dialog_type;
-
         //! The message catalog type.
         using message_catalog_type = typename traits_type::message_catalog_type;
-
-        //! The timetable file encoder type.
-        using timetable_file_encoder_type = typename traits_type::timetable_file_encoder_type;
-
-        //! The WinDIA file encoder type.
-        using windia_file_encoder_type = typename traits_type::windia_file_encoder_type;
 
         //! The model type.
         using model_type =
@@ -111,46 +66,6 @@ namespace bobura { namespace load_save
 
         //! The file save confirmation type.
         using confirm_file_save_type = confirm_file_save<traits_type>;
-
-        //! The reader selector type.
-        using reader_selector_type =
-            model::serializer::reader_selector<
-                size_type, difference_type, string_type, iterator, operating_distance_type, speed_type, font_type
-            >;
-
-        //! The JSON reading task execution type.
-        using exec_json_reading_task_type =
-            model::serializer::exec_json_reading_task<
-                size_type,
-                difference_type,
-                string_type,
-                operating_distance_type,
-                speed_type,
-                font_type,
-                dialog_type,
-                timer_type,
-                system_color_set_type,
-                message_catalog_type
-            >;
-
-        //! The OuDia diagram selector type.
-        using select_oudia_diagram_type = model::serializer::select_oudia_diagram<oudia_diagram_dialog_type>;
-
-        //! The reader set type.
-        using reader_set_type =
-            model::serializer::reader_set<
-                size_type,
-                difference_type,
-                string_type,
-                iterator,
-                operating_distance_type,
-                speed_type,
-                exec_json_reading_task_type,
-                select_oudia_diagram_type,
-                font_type,
-                timetable_file_encoder_type,
-                windia_file_encoder_type
-            >;
 
 
         // constructors and destructor
@@ -166,12 +81,13 @@ namespace bobura { namespace load_save
             const bool                    ask_file_path,
             const confirm_file_save_type& confirm_file_save,
             const message_catalog_type&   message_catalog
-        )
-        :
-        m_ask_file_path(ask_file_path),
-        m_confirm_file_save(confirm_file_save),
-        m_message_catalog(message_catalog)
-        {}
+        );
+
+        /*!
+            \brief Destroys the file loading.
+        */
+        ~load_from_file()
+        TETENGO2_STDALT_NOEXCEPT;
 
 
         // functions
@@ -186,10 +102,7 @@ namespace bobura { namespace load_save
             \retval false Otherwise.
         */
         bool reloadable(const model_type& model, const boost::optional<boost::filesystem::path>& given_path)
-        const
-        {
-            return m_ask_file_path || model.has_path() || given_path;
-        }
+        const;
 
         /*!
             \brief Executes the load_save.
@@ -203,178 +116,18 @@ namespace bobura { namespace load_save
             const boost::optional<boost::filesystem::path>& given_path,
             abstract_window_type&                           parent
         )
-        const
-        {
-            if (!m_ask_file_path && !model.has_path() && !given_path)
-                return;
-
-            if (!m_confirm_file_save(parent))
-                return;
-
-            boost::filesystem::path path{};
-            if (given_path)
-            {
-                path = *given_path;
-            }
-            else if (m_ask_file_path)
-            {
-                file_open_dialog_type dialog{
-                    m_message_catalog.get(TETENGO2_TEXT("Dialog:FileOpenSave:Open")), make_file_filters(), parent
-                };
-                const auto ok = dialog.do_modal();
-                if (!ok)
-                    return;
-
-                path = dialog.result();
-            }
-            else
-            {
-                assert(model.has_path());
-                path = model.path();
-            }
-
-            boost::filesystem::ifstream input_stream{ path, std::ios_base::binary };
-            if (!input_stream)
-            {
-                create_cant_open_file_message_box(path, parent)->do_modal();
-                return;
-            }
-
-            reader_selector_type reader_selector{
-                reader_set_type::create_readers(parent, path.template string<string_type>(), m_message_catalog)
-            };
-            const auto first =
-                tetengo2::make_observable_forward_iterator(
-                    boost::spirit::make_default_multi_pass(std::istreambuf_iterator<char>{ input_stream })
-                );
-            const auto last =
-                tetengo2::make_observable_forward_iterator(
-                    boost::spirit::make_default_multi_pass(std::istreambuf_iterator<char>{})
-                );
-            auto error = reader_error_type::none;
-            auto p_timetable = reader_selector.read(first, last, error);
-            if (!p_timetable)
-            {
-                switch (error)
-                {
-                case reader_error_type::canceled:
-                    break; // Do nothing.
-                case reader_error_type::corrupted:
-                    create_file_broken_message_box(path, parent)->do_modal();
-                    break;
-                case reader_error_type::unsupported:
-                    create_unsupported_format_file_message_box(path, parent)->do_modal();
-                    break;
-                default:
-                    assert(false);
-                    BOOST_THROW_EXCEPTION(std::logic_error("Unknown reader error."));
-                }
-                return;
-            }
-
-            model.reset_timetable(std::move(p_timetable), path);
-        }
+        const;
 
 
     private:
         // types
 
-        using reader_error_type = typename reader_selector_type::error_type;
+        class impl;
 
 
         // variables
 
-        const bool m_ask_file_path;
-
-        const confirm_file_save_type& m_confirm_file_save;
-
-        const message_catalog_type& m_message_catalog;
-
-
-        // functions
-
-        std::unique_ptr<message_box_type> create_cant_open_file_message_box(
-            const boost::filesystem::path& path,
-            abstract_window_type&          parent
-        )
-        const
-        {
-            return
-                tetengo2::stdalt::make_unique<message_box_type>(
-                    parent,
-                    m_message_catalog.get(TETENGO2_TEXT("App:Bobura")),
-                    m_message_catalog.get(TETENGO2_TEXT("Message:File:Can't open the file.")),
-                    path.template string<string_type>(),
-                    message_box_type::button_style_type::ok(false),
-                    message_box_type::icon_style_type::error
-                );
-        }
-
-        std::unique_ptr<message_box_type> create_file_broken_message_box(
-            const boost::filesystem::path& path,
-            abstract_window_type&          parent
-        )
-        const
-        {
-            return
-                tetengo2::stdalt::make_unique<message_box_type>(
-                    parent,
-                    m_message_catalog.get(TETENGO2_TEXT("App:Bobura")),
-                    m_message_catalog.get(TETENGO2_TEXT("Message:File:The timetable file is corrupted.")),
-                    path.template string<string_type>(),
-                    message_box_type::button_style_type::ok(false),
-                    message_box_type::icon_style_type::error
-                );
-        }
-
-        std::unique_ptr<message_box_type> create_unsupported_format_file_message_box(
-            const boost::filesystem::path& path,
-            abstract_window_type&          parent
-        )
-        const
-        {
-            return
-                tetengo2::stdalt::make_unique<message_box_type>(
-                    parent,
-                    m_message_catalog.get(TETENGO2_TEXT("App:Bobura")),
-                    m_message_catalog.get(TETENGO2_TEXT("Message:File:Unsupported format file.")),
-                    path.template string<string_type>(),
-                    message_box_type::button_style_type::ok(false),
-                    message_box_type::icon_style_type::error
-                );
-        }
-
-        typename file_open_dialog_type::file_filters_type make_file_filters()
-        const
-        {
-            return
-                {
-                    {
-                        m_message_catalog.get(TETENGO2_TEXT("Dialog:FileOpenSave:All Timetable Files")),
-                        string_type{ TETENGO2_TEXT("btt;*.btt_bz2;*.oud;*.dia") }
-                    },
-                    {
-                        m_message_catalog.get(TETENGO2_TEXT("Dialog:FileOpenSave:Timetable Files")),
-                        string_type{ TETENGO2_TEXT("btt") }
-                    },
-                    {
-                        m_message_catalog.get(TETENGO2_TEXT("Dialog:FileOpenSave:Timetable Files (Compressed)")),
-                        string_type{ TETENGO2_TEXT("btt_bz2") }
-                    },
-                    {
-                        m_message_catalog.get(TETENGO2_TEXT("Dialog:FileOpenSave:OuDia Files")),
-                        string_type{ TETENGO2_TEXT("oud") }
-                    },
-                    {
-                        m_message_catalog.get(TETENGO2_TEXT("Dialog:FileOpenSave:WinDIA Files")),
-                        string_type{ TETENGO2_TEXT("dia") }
-                    },
-                    {
-                        m_message_catalog.get(TETENGO2_TEXT("Dialog:FileOpenSave:All Files")),
-                        string_type{ TETENGO2_TEXT("*") }
-                    },
-                };
-        }
+        const std::unique_ptr<impl> m_p_impl;
 
 
     };
