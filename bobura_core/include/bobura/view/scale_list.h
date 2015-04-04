892320635600de5de135dc12a95e68342eb07f5f
kaoru <kaorut@mc.pikara.ne.jp>
2015-04-04 10:19:33 +0900

    Move the implementation of bobura::view::scale_list to .cpp #99

--- bobura_core/include/bobura/view/scale_list.h
+++ bobura_core/include/bobura/view/scale_list.h
@@ -9,15 +9,11 @@
 #if !defined(BOBURA_VIEW_SCALELIST_H)
 #define BOBURA_VIEW_SCALELIST_H
 
-#include <algorithm>
-#include <cassert>
-#include <functional>
+#include <memory>
 #include <stdexcept>
-#include <vector>
 
 #include <boost/core/noncopyable.hpp>
 #include <boost/optional.hpp>
-#include <boost/throw_exception.hpp>
 
 #include <tetengo2.h>
 
@@ -32,7 +28,7 @@ namespace bobura { namespace view
         \tparam Scale  A scale type.
     */
     template <typename Size, typename String, typename Scale>
-    class scale_list : boost::noncopyable
+    class scale_list : private boost::noncopyable
     {
     public:
         // types
@@ -47,6 +43,19 @@ namespace bobura { namespace view
         using scale_type = Scale;
 
 
+        // constructors and destructor
+
+        /*!
+            \brief Creates a view scale list.
+        */
+        scale_list();
+
+        /*!
+            \brief Destroys the view scale list.
+        */
+        ~scale_list();
+
+
         // functions
 
         /*!
@@ -55,11 +64,7 @@ namespace bobura { namespace view
             \return The scale count.
         */
         size_type size()
-        const
-        {
-            assert(scales().size() == labels().size());
-            return scales().size();
-        }
+        const;
 
         /*!
             \brief Returns the scale at the specified index.
@@ -71,13 +76,7 @@ namespace bobura { namespace view
             \throw std::out_of_range When index is out of range.
         */
         const scale_type& at(const size_type index)
-        const
-        {
-            if (index >= scales().size())
-                BOOST_THROW_EXCEPTION(std::out_of_range("index is out of range."));
-
-            return scales()[index];
-        }
+        const;
 
         /*!
             \brief Returns the scale label at the specified index.
@@ -89,13 +88,7 @@ namespace bobura { namespace view
             \throw std::out_of_range When index is out of range.
         */
         const string_type& label_at(const size_type index)
-        const
-        {
-            if (index >= labels().size())
-                BOOST_THROW_EXCEPTION(std::out_of_range("index is out of range."));
-
-            return labels()[index];
-        }
+        const;
 
         /*!
             \brief Returns the same scale index.
@@ -105,15 +98,7 @@ namespace bobura { namespace view
             \return The same scale index.
         */
         boost::optional<size_type> index_of(const scale_type& scale)
-        const
-        {
-            for (size_type i = 0; i < scales().size(); ++i)
-            {
-                if (scales()[i] == scale)
-                    return boost::make_optional(i);
-            }
-            return boost::none;
-        }
+        const;
 
         /*!
             \brief Returns the nearest larger scale.
@@ -123,16 +108,7 @@ namespace bobura { namespace view
             \return The nearest larger scale.
         */
         const scale_type& larger(const scale_type& scale)
-        const
-        {
-            const auto found =
-                std::find_if(
-                    scales().begin(),
-                    scales().end(),
-                    [&scale](const scale_type& candidate) { return candidate > scale; }
-                );
-            return found != scales().end() ? *found : scales().back();
-        }
+        const;
 
         /*!
             \brief Returns the nearest smaller scale.
@@ -142,66 +118,18 @@ namespace bobura { namespace view
             \return The nearest smaller scale.
         */
         const scale_type& smaller(const scale_type& scale)
-        const
-        {
-            const auto found =
-                std::find_if(
-                    scales().rbegin(),
-                    scales().rend(),
-                    [&scale](const scale_type& candidate) { return candidate < scale; }
-                );
-            return found != scales().rend() ? *found : scales().front();
-        }
+        const;
 
 
     private:
-        // static functions
+        // types
 
-        static const std::vector<scale_type>& scales()
-        {
-            static const std::vector<scale_type> singleton{ make_scales() };
-            return singleton;
-        }
+        class impl;
 
-        static const std::vector<string_type>& labels()
-        {
-            static const std::vector<string_type> singleton{ make_labels() };
-            return singleton;
-        }
 
-        static std::vector<scale_type> make_scales()
-        {
-            std::vector<scale_type> scales{
-                { 1, 10 },
-                { 1, 4 },
-                { 1, 2 },
-                { 3, 4 },
-                { 1 },
-                { 3, 2 },
-                { 2 },
-                { 4 },
-            };
+        // variables
 
-            assert(std::is_sorted(scales.begin(), scales.end(), std::less<scale_type>{}));
-            return scales;
-        }
-
-        static std::vector<string_type> make_labels()
-        {
-            std::vector<string_type> labels{
-                { string_type{ TETENGO2_TEXT("10%") } },
-                { string_type{ TETENGO2_TEXT("25%") } },
-                { string_type{ TETENGO2_TEXT("50%") } },
-                { string_type{ TETENGO2_TEXT("75%") } },
-                { string_type{ TETENGO2_TEXT("100%") } },
-                { string_type{ TETENGO2_TEXT("150%") } },
-                { string_type{ TETENGO2_TEXT("200%") } },
-                { string_type{ TETENGO2_TEXT("400%") } },
-            };
-
-            assert(labels.size() == scales().size());
-            return labels;
-        }
+        const std::unique_ptr<impl> m_p_impl;
 
 
     };
