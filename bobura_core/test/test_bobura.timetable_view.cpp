385a00e6a6d6633d2e1b59f966dbe59f44565fd6
kaoru <kaorut@mc.pikara.ne.jp>
2015-08-11 03:03:35 +0900

    Create test_bobura::timetable_view #102

---
+++ bobura_core/test/test_bobura.timetable_view.cpp
@@ -0,0 +1,103 @@
+/*! \file
+    \brief Test of class bobura::timetable_view.
+
+    Copyright (C) 2007-2015 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include <tetengo2.h>
+#include <tetengo2.gui.h>
+
+#include <bobura/timetable_view.h>
+#include <bobura/timetable_model.h>
+#include <bobura/type_list.h>
+
+
+namespace
+{
+    // types
+
+    using detail_type_list_type = bobura::type_list::detail_for_test;
+
+    using common_type_list_type = bobura::type_list::common;
+
+    using locale_type_list_type = bobura::type_list::locale<detail_type_list_type>;
+
+    using ui_type_list_type = bobura::type_list::ui<detail_type_list_type>;
+
+    using traits_type_list_type = bobura::type_list::traits<detail_type_list_type>;
+
+    using model_type =
+        bobura::timetable_model<
+            common_type_list_type::size_type,
+            common_type_list_type::difference_type,
+            common_type_list_type::string_type,
+            common_type_list_type::operating_distance_type,
+            common_type_list_type::speed_type,
+            ui_type_list_type::fast_font_type
+        >;
+
+    using window_type = ui_type_list_type::window_type;
+
+    using picture_box_type = ui_type_list_type::picture_box_type;
+
+    using position_type = picture_box_type::position_type;
+
+    using left_type = tetengo2::gui::position<position_type>::left_type;
+
+    using top_type = tetengo2::gui::position<position_type>::top_type;
+
+    using dimension_type = picture_box_type::dimension_type;
+
+    using width_type = tetengo2::gui::dimension<dimension_type>::width_type;
+
+    using height_type = tetengo2::gui::dimension<dimension_type>::height_type;
+
+    using message_catalog_type = locale_type_list_type::message_catalog_type;
+
+    using timetable_view_traits_type = traits_type_list_type::timetable_view_type;
+
+    using timetable_view_type = bobura::timetable_view<timetable_view_traits_type>;
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(timetable_view)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const model_type model{};
+        const message_catalog_type message_catalog{};
+        const timetable_view_type timetable_view{ model, message_catalog };
+    }
+
+    BOOST_AUTO_TEST_CASE(draw_on)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const model_type model{};
+        const message_catalog_type message_catalog{};
+        timetable_view_type timetable_view{ model, message_catalog };
+
+        window_type window{};
+        const picture_box_type picture_box{ window, picture_box_type::scroll_bar_style_type::both };
+        const auto p_canvas = picture_box.create_canvas();
+        timetable_view.draw_on(
+            *p_canvas,
+            dimension_type{ width_type{ 42 }, height_type{ 24 } },
+            position_type{ left_type{ 2 }, top_type{ 3 } }
+        );
+    }
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
