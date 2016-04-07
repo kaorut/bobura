4aab0f61a10924813836fa7388770d2332b4f90e
kaoru <kaorut@mc.pikara.ne.jp>
2016-04-08 03:14:43 +0900

    Create bobura::view::timetable::train_number_header #102

---
+++ bobura_core/include/bobura/view/timetable/train_number_header.h
@@ -0,0 +1,147 @@
+/*! \file
+    \brief The definition of bobura::view::timetable::train_number_header.
+
+    Copyright (C) 2007-2016 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_VIEW_TIMETABLE_TRAINNUMBERHEADER_H)
+#define BOBURA_VIEW_TIMETABLE_HEADER_H
+
+#include <memory>
+
+#include <tetengo2.h>
+
+#include <bobura/timetable_model.h>
+#include <bobura/view/timetable/item.h>
+#include <bobura/view/timetable/utility.h>
+
+
+namespace bobura { namespace view { namespace timetable
+{
+    /*!
+        \brief The class template for a train number header in the timetable view.
+
+        \tparam Traits A traits type.
+    */
+    template <typename Traits>
+    class train_number_header : public item<Traits>
+    {
+    public:
+        // types
+
+        //! The traits type.
+        using traits_type = Traits;
+
+        //! The size type.
+        using size_type = typename traits_type::size_type;
+
+        //! The difference type.
+        using difference_type = typename traits_type::difference_type;
+
+        //! The string type.
+        using string_type = typename traits_type::string_type;
+
+        //! The operating distance type.
+        using operating_distance_type = typename traits_type::operating_distance_type;
+
+        //! The speed type.
+        using speed_type = typename traits_type::speed_type;
+
+        //! The canvas type.
+        using canvas_type = typename traits_type::canvas_type;
+
+        //! The dimension type.
+        using dimension_type = typename canvas_type::dimension_type;
+
+        //! The font type.
+        using font_type = typename canvas_type::font_type;
+
+        //! The message catalog type.
+        using message_catalog_type = typename traits_type::message_catalog_type;
+
+        //! The model type.
+        using model_type =
+            timetable_model<size_type, difference_type, string_type, operating_distance_type, speed_type, font_type>;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a train number header.
+
+            \param direction        A direction.
+            \param model            A model.
+            \param message_catalog  A message catalog.
+            \param canvas           A canvas.
+            \param canvas_dimension A canvas dimension.
+        */
+        train_number_header(
+            direction_type              direction,
+            const model_type&           model,
+            const message_catalog_type& message_catalog,
+            canvas_type&                canvas,
+            const dimension_type&       canvas_dimension
+        );
+
+        /*!
+            \brief Moves a train number header.
+
+            \param another Another train number header
+        */
+        train_number_header(train_number_header&& another);
+
+        /*!
+            \brief Destroys the train number header.
+        */
+        virtual ~train_number_header()
+        noexcept;
+
+
+        // functions
+
+        /*!
+            \brief Assigns a train number header.
+
+            \param another Another train number header.
+
+            \return This train number header.
+        */
+        train_number_header& operator=(train_number_header&& another);
+
+        /*!
+            \brief Returns the dimension.
+
+            \return The dimension.
+        */
+        const dimension_type& dimension()
+        const;
+
+
+    private:
+        // types
+
+        using base_type = item<traits_type>;
+
+        class impl;
+
+
+        // variables
+
+        const std::unique_ptr<impl> m_p_impl;
+
+
+        // virtual functions
+
+        virtual void draw_on_impl(canvas_type& canvas)
+        const override;
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ bobura_core/src/bobura.view.timetable.train_number_header.cpp
@@ -0,0 +1,204 @@
+/*! \file
+    \brief The definition of bobura::view::timetable::train_number_header.
+
+    Copyright (C) 2007-2016 kaoru
+
+    $Id$
+*/
+
+#include <utility>
+
+#include <boost/core/noncopyable.hpp>
+#include <boost/predef.h>
+
+#include <tetengo2.h>
+#include <tetengo2.gui.h>
+
+#include <bobura/type_list.h>
+#include <bobura/view/timetable/train_number_header.h>
+#include <bobura/view/timetable/utility.h>
+
+
+namespace bobura { namespace view { namespace timetable
+{
+    template <typename Traits>
+    class train_number_header<Traits>::impl : private boost::noncopyable
+    {
+    public:
+        // types
+
+        using traits_type = Traits;
+
+        using canvas_type = typename traits_type::canvas_type;
+
+        using dimension_type = typename canvas_type::dimension_type;
+
+        using message_catalog_type = typename traits_type::message_catalog_type;
+
+        using model_type = typename train_number_header::model_type;
+
+
+        // constructors and destructor
+
+        impl(
+            const direction_type        /*direction*/,
+            const model_type&           model,
+            const message_catalog_type& /*message_catalog*/,
+            canvas_type&                /*canvas*/,
+            const dimension_type&       /*canvas_dimension*/
+        )
+        :
+        m_p_ruled_line_color(&*model.timetable().font_color_set().ruled_line().timetable_color()),
+        m_position(left_type{ 0 }, top_type{ 0 }),
+        m_dimension(width_type{ 0 }, height_type{ 0 })
+        {
+
+        }
+
+        impl(impl&& another)
+        :
+        m_p_ruled_line_color(another.m_p_ruled_line_color),
+        m_position(std::move(another.m_position)),
+        m_dimension(std::move(another.m_dimension))
+        {}
+
+
+        // functions
+
+        impl& operator=(impl&& another)
+        {
+            if (&another == this)
+                return *this;
+
+            m_p_ruled_line_color = another.m_p_ruled_line_color;
+            m_position = std::move(another.m_position);
+            m_dimension = std::move(another.m_dimension);
+
+            return *this;
+        }
+
+        const dimension_type& dimension()
+        const
+        {
+            return m_dimension;
+        }
+
+        void draw_on_impl(canvas_type& /*canvas*/)
+        const
+        {
+
+        }
+
+
+    private:
+        // types
+
+        using string_type = typename traits_type::string_type;
+
+        using color_type = typename canvas_type::color_type;
+
+        using position_type = typename canvas_type::position_type;
+
+        using left_type = typename tetengo2::gui::position<position_type>::left_type;
+
+        using top_type = typename tetengo2::gui::position<position_type>::top_type;
+
+        using width_type = typename tetengo2::gui::dimension<dimension_type>::width_type;
+
+        using height_type = typename tetengo2::gui::dimension<dimension_type>::height_type;
+
+
+        // variables
+
+        const color_type* m_p_ruled_line_color;
+
+        position_type m_position;
+
+        dimension_type m_dimension;
+
+
+    };
+
+
+    template <typename Traits>
+    train_number_header<Traits>::train_number_header(
+        const direction_type        direction,
+        const model_type&           model,
+        const message_catalog_type& message_catalog,
+        canvas_type&                canvas,
+        const dimension_type&       canvas_dimension
+    )
+    :
+    base_type(),
+    m_p_impl(tetengo2::stdalt::make_unique<impl>(direction, model, message_catalog, canvas, canvas_dimension))
+    {}
+
+    template <typename Traits>
+    train_number_header<Traits>::train_number_header(train_number_header&& another)
+    :
+    base_type(),
+    m_p_impl(tetengo2::stdalt::make_unique<impl>(std::move(*another.m_p_impl)))
+    {}
+
+    template <typename Traits>
+    train_number_header<Traits>::~train_number_header()
+    noexcept
+    {}
+
+    template <typename Traits>
+    train_number_header<Traits>& train_number_header<Traits>::operator=(train_number_header&& another)
+    {
+        if (&another == this)
+            return *this;
+
+        *m_p_impl = std::move(*another.m_p_impl);
+        base_type::operator=(std::move(another));
+
+        return *this;
+    }
+
+    template <typename Traits>
+    const typename train_number_header<Traits>::dimension_type& train_number_header<Traits>::dimension()
+    const
+    {
+        return m_p_impl->dimension();
+    }
+
+    template <typename Traits>
+    void train_number_header<Traits>::draw_on_impl(canvas_type& canvas)
+    const
+    {
+        m_p_impl->draw_on_impl(canvas);
+    }
+
+        
+    namespace
+    {
+#if BOOST_COMP_MSVC
+        namespace application
+        {
+            using detail_type_list_type = type_list::detail_for_application;
+
+            using traits_type_list_type = type_list::traits<detail_type_list_type>;
+
+        }
+#endif
+
+        namespace test
+        {
+            using detail_type_list_type = type_list::detail_for_test;
+
+            using traits_type_list_type = type_list::traits<detail_type_list_type>;
+
+        }
+
+    }
+
+#if BOOST_COMP_MSVC
+    template class train_number_header<typename application::traits_type_list_type::timetable_view_type>;
+#endif
+
+    template class train_number_header<typename test::traits_type_list_type::timetable_view_type>;
+
+
+}}}
